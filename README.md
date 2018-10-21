6be42d3a4d1dba1b3275472dfd76d4ff6f6e56fa
Luke Burgess <luke.burgess94@gmail.com>
2018-10-21 20:53:17 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # aws-transcribe-interpreter
 A simple script that turns AWS Transcribe JSON output into human readable text files.
+
+Takes audio with exactly two speakers and outputs a text file with the following format:
+
+```
+(HH:mm:ss) Speaker One: The transcribed text of the first thing said by the first speaker.
+
+(HH:mm:ss) Speaker Two: The transcribed text of the first thing said by the second speaker.
+
+... etc.
+```
+
+This script currently has no error handling and the code can probably be optimised a lot more, but it gets the job done.
---
+++ interpreter.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+const inputFile = 'transcriptions/Interview.json';
+const outputFile = 'transcriptions/Transcribed.txt';
+const host = 'Interviewer';
+const guest = 'Interviewee';
+const hostSpeaksFirst = true;
+
+const rawdata = fs.readFileSync(inputFile);
+const data = JSON.parse(rawdata);
+
+const rawSegments = data.results.speaker_labels.segments.map(x => ({
+  speaker: x.speaker_label === (hostSpeaksFirst ? 'spk_0' : 'spk_1') ? host : guest,
+  start: x.start_time,
+  end: x.end_time
+}));
+const rawText = data.results.items.map(x => ({
+  start: x.start_time,
+  end: x.end_time,
+  word: x.alternatives[0].content,
+  punctuation: x.type === 'punctuation'
+}));
+
+const segments = rawSegments.reduce((acc, val) => {
+  const currentSegment = acc[acc.length - 1];
+  if (val.speaker === currentSegment.speaker) {
+    acc[acc.length - 1] = {
+      speaker: currentSegment.speaker,
+      start: currentSegment.start,
+      end: val.end
+    }
+    return acc;
+  } else {
+    acc.push({
+      speaker: val.speaker,
+      start: val.start,
+      end: val.end
+    });
+    return acc;
+  }
+}, [rawSegments[0]]);
+
+const jsonOutput = segments.reduce((acc, val) => {
+  const newText = rawText
+    .filter(x => x.start >= Number(val.start) && x.end <= Number(val.end))
+    .reduce((a, v) => {
+      if (v.punctuation) {
+        return a += v.word;
+      } else {
+        return a += ` ${v.word}`;
+      }
+    }, "");
+
+  let date = new Date(null);
+  date.setSeconds(val.start);
+  const time = date.toISOString().substr(11, 8);
+
+  acc.push({
+    speaker: val.speaker,
+    start: time,
+    text: newText
+  });
+  return acc;
+}, []);
+
+const fileText = jsonOutput.map(x => `(${x.start}) ${x.speaker}:${x.text}`).join('. \n\n') + '.';
+
+fs.writeFile(outputFile, fileText, function (err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("The file was saved!");
+});
